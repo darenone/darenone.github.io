627096e362b39712ce199dd34f4ce6c7de5fecbd
宗强 <1291342008@qq.com>
2024-02-04 15:08:42 +0800

    Description：配置代碼自動格式化 FeatureID：dev分支 IssueID：

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  endOfLine: 'auto',
+	// 书写宽度
+	printWidth: 80,
+	// 语句末尾打印分号
+	semi: true,
+	// 使用单引号
+	singleQuote: true,
+	// 尾随逗号
+	trailingComma: 'none',
+	// 在方法的花括号前面加空格
+	spaceBeforeFunctionParen: true,
+	// 用键位tab缩进
+	useTabs: true,
+	// 标签换行不完整问题
+	htmlWhitespaceSensitivity: 'ignore',
+	// 在唯一的箭头函数参数周围包含括号
+	arrowParens: 'always'
+}
